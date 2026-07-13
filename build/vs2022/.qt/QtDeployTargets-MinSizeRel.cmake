3ef8190c44c95d8f6fc2acc29c6e60d1322be97b
czl <2312532559@qq.com>
2026-07-13 09:48:06 +0800

    Clean: remove tracked build/out/.vs dirs from Git index

--- build/vs2022/.qt/QtDeployTargets-MinSizeRel.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_CommBenchPro_FILE E:/code/QT/SerialEvaluation/build/vs2022/MinSizeRel/CommBenchPro.exe)
-set(__QT_DEPLOY_TARGET_CommBenchPro_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_CommBenchPro_RUNTIME_DLLS D:/Soft/QT/Qt/6.8.3/msvc2022_64/bin/Qt6Widgets.dll;D:/Soft/QT/Qt/6.8.3/msvc2022_64/bin/Qt6Network.dll;D:/Soft/QT/Qt/6.8.3/msvc2022_64/bin/Qt6SerialPort.dll;D:/Soft/QT/Qt/6.8.3/msvc2022_64/bin/Qt6Gui.dll;D:/Soft/QT/Qt/6.8.3/msvc2022_64/bin/Qt6Core.dll)
